013545c0c585d8494ebd4a7d38f9628f2e6c7543
kingsley <alexkingsley36@gmail.com>
2024-08-13 13:16:25 +0100

    initial commit

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,6 @@ connectDB();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Set EJS as the template engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
 // Serve static files
 app.use(express.static('public'));
 
@@ -27,16 +23,6 @@ app.use((req: Request, _res: Response, next: NextFunction) => {
   console.log('Request body:', req.body);
   next();
 });
-
-// Define a route to render the EJS template
-app.get('/', (req, res) => {
-  res.render('index', { originalUrl: '', errorMessage: '' });
-});
-
-
-// Serve static files (if any)
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 // API routes
 app.use('/api', urlRoutes);
